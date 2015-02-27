01e4b598eecf26b269997365afed5ac582f96331
Zheng Hao Tan <tanzhao@umich.edu>
2015-02-27 17:56:25 -0500

    scaling of buttons are working

--- Kalkulator/ViewController.swift
+++ Kalkulator/ViewController.swift
@@ -22,7 +22,7 @@ class ViewController: UIViewController {
 
     @IBOutlet weak var display: UILabel!
 
-    var userIsInTheMiddleOfTypingANumber: Bool = false;
+    var userIsInTheMiddleOfTypingANumber = false;
     
     @IBAction func appendDigit(sender: UIButton) {
         let digit = sender.currentTitle!;
@@ -38,9 +38,56 @@ class ViewController: UIViewController {
         println("digit: \(digit)");
     }
     
+    @IBAction func operate(sender: UIButton) {
+        let operation = sender.currentTitle!;
+        
+        if userIsInTheMiddleOfTypingANumber {
+            enter();
+        }
+
+        switch operation {
+            
+        case "✕": performOperation { $0 * $1 };
+        case "÷": performOperation { $1 / $0 };
+        case "+": performOperation { $0 + $1 };
+        case "-": performOperation { $1 - $0 };
+        case "√": performOperation { sqrt($0) };
+        default: break;
+        }
+    }
+    
+    func performOperation(operation: (Double, Double) -> Double) {
+        if (operandStack.count >= 2) {
+            displayValue = operation(operandStack.removeLast(), operandStack.removeLast());
+            enter();
+        }
+    }
+
+    func performOperation(operation: Double -> Double) {
+        if (operandStack.count >= 1) {
+            displayValue = operation(operandStack.removeLast());
+            enter();
+        }
+    }
+
+    // Stack that stores the numbers
+    var operandStack = Array<Double>();
+    
     @IBAction func enter() {
         userIsInTheMiddleOfTypingANumber = false;
-        
+        operandStack.append(displayValue);
+        println("operandStack:  \(operandStack)");
     }
+
+    var displayValue: Double {
+        get {
+            return NSNumberFormatter().numberFromString(display.text!)!.doubleValue;
+        }
+        set {
+            display.text = "\(newValue)";
+            userIsInTheMiddleOfTypingANumber = false;
+        }
+    }
+
 }
 
