bc3ce7b356267cb44b6f7da8b9ca89a2f6c7f473
Zheng Hao Tan <tanzhao@umich.edu>
2015-02-26 21:48:04 -0500

    Initial Commit

---
+++ Kalkulator/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Kalkulator
+//
+//  Created by Zheng Hao Tan on 2/26/15.
+//  Copyright (c) 2015 Zheng Hao Tan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
